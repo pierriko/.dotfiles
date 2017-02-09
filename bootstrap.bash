335775c3c86494c8db942bb40ea8f4702e1dbc55
Pierrick Koch <pierrick.koch@easymile.com>
2017-02-09 11:26:00 +0100

    [bootstrap] diff dep latex vs. work

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo "Bootstrap 2017 Fedora 25 x64"
 
-list="biber fontforge-devel gdal-devel gdal-python gimp git-svn htop nmap \
+dep_latex="biber rubber texlive-alg texlive-algorithm2e texlive-algorithmicx \
+texlive-framed texlive-minitoc texlive-minted texlive-multirow texlive-nomencl \
+texlive-pgfplots texlive-shadowtext texlive-standalone texlive-subfigure \
+texlive-tablefootnote texlive-textpos texlive-todonotes"
+
+dep_work="fontforge-devel gdal-devel gdal-python gimp git-svn htop nmap \
 opencv-devel opencv-python pcl-devel pcl-tools python-devel python-requests \
-recordmydesktop rubber ruby-devel terminator texlive-alg texlive-algorithm2e \
-texlive-algorithmicx texlive-framed texlive-minitoc texlive-minted texlive-multirow \
-texlive-nomencl texlive-pgfplots texlive-shadowtext texlive-standalone \
-texlive-subfigure texlive-tablefootnote texlive-textpos texlive-todonotes vim"
+recordmydesktop ruby-devel terminator vim"
 
 sudo dnf update --refresh
-sudo dnf install $list
+sudo dnf install $dep_work $dep_latex
 
 # https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet
 cd ~/.dotfiles
